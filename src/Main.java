f11d5f506f191cdcead2037f1e149476c8b170b7
UPUHAK <89092936911@mail.ru>
2024-10-06 23:49:33 +0300

    homework is done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import exception.WrongLoginException;
+import exception.WrongPasswordException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String login = "Login";
+        String password = "password123";
+        String confirmPassword = "password123";
+
+        try {
+            Validator.validate(login, password, confirmPassword);
+        } catch (WrongLoginException e) {
+            System.out.println("Произошла ошибка при проверке логина: " + e.getMessage());
+        } catch (WrongPasswordException e) {
+            System.out.println("Произошла ошибка при проверке пароля: " + e.getMessage());
+        } finally {
+            System.out.println("Валидация завершена.");
+        }
+
     }
 }
